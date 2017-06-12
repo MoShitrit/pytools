b0f4da8ceb527b52fbd8d268f55fb704ddac4a7c
Moshe Shitrit <moshe1750@gmail.com>
2017-06-12 16:20:06 -0400

    Added install_package method

--- lib/local_exec.py
+++ lib/local_exec.py
@@ -4,8 +4,7 @@ This module contains the LocalExec class, which is used for local commands execu
 """
 
 import subprocess
-from color import Color
-import os
+import platform
 
 __author__ = 'Moshe Shitrit'
 __creation_date__ = '11/30/16'
@@ -13,8 +12,9 @@ __creation_date__ = '11/30/16'
 
 class LocalExec:
 
-    def __init__(self):
-        pass
+    def __init__(self, logger=None):
+        self.distro = platform.dist()[0]
+        self.logger = logger
 
     @staticmethod
     def run_shell_cmd(cmd):
@@ -41,4 +41,31 @@ class LocalExec:
             return False
         return True
 
+    def install_package(self, package):
+        """
+        Install the provided package name using the appropriate package manager according to the distro
+        :param package: name of package(s) to install
+        :return: True if succeeded, False if not
+        """
+        if self.logger:
+            self.logger.info('Going to install package(s): {0}'.format(package))
+        if self.distro in ['centos', 'redhat']:
+            if self.logger:
+                self.logger.debug('OS distro is {0}, going to use yum package manager for installation'.format(self.distro))
+            output = self.run_shell_cmd('sudo yum install -y {0}'.format(package))
+        elif self.distro in ['Ubuntu', 'Debian']:
+            if self.logger:
+                self.logger.debug('OS distro is {0}, going to use apt package manager for installation'.format(self.distro))
+            output = self.run_shell_cmd('sudo apt-get install -y {0}'.format(package))
+        else:
+            if self.logger:
+                self.logger.error('OS distribution is not supported! Value for self.distro is: {0}'.format(self.distro))
+            return False
 
+        if output['ret_code'] != 0:
+            if self.logger:
+                self.logger.error('{0} package installation failed with error {1}: {2}'.format(package,
+                                                                                               output['ret_code'],
+                                                                                               output['stderr']))
+            return False
+        return True
